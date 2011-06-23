01cc725189b1f3c665925dee25c4c0aebb9ad63c
Simon Osborne <flibble@gmail.com>
2011-06-23 19:58:52 +0100

    changes to smartass as suggested by genokolar

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -136,13 +136,13 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 60
+#define DEFAULT_MAX_CPU_LOAD 50
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 30
+#define DEFAULT_MIN_CPU_LOAD 25
 static unsigned long min_cpu_load;
 
 
