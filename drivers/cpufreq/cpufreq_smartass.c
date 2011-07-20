c55494e7356ec9882e8469c50c0e030b891c1e33
Simon Osborne <flibble@gmail.com>
2011-07-20 16:13:19 +0100

    arm: Allow CPU-supported unaligned accesses

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -110,7 +110,7 @@ static unsigned int sleep_wakeup_freq;
  * go below this frequency.
  * Set awake_min_freq=0 to disable this behavior.
  */
-#define DEFAULT_AWAKE_MIN_FREQ 245760
+#define DEFAULT_AWAKE_MIN_FREQ 122880
 static unsigned int awake_min_freq;
 
 /*
@@ -123,7 +123,7 @@ static unsigned int sample_rate_jiffies;
  * Freqeuncy delta when ramping up.
  * zero disables and causes to always jump straight to max frequency.
  */
-#define DEFAULT_RAMP_UP_STEP 256000
+#define DEFAULT_RAMP_UP_STEP 0
 static unsigned int ramp_up_step;
 
 /*
